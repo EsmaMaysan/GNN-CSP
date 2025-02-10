7368f2f5f806f35ab3a81c1b86c5d0f4b9eeb59b
AsmaaCherkaoui <esma1maysan@gmail.com>
2025-02-10 13:25:09 +0100

    Update FirstSolQueenGraphPuzzle.py

--- FirstSolQueenGraphPuzzle.py
+++ FirstSolQueenGraphPuzzle.py
@@ -9,7 +9,7 @@ def initial_solution(n):
         board[0, 0] = 1
         return board
     if n in [2, 3]:
-        raise ValueError("No Solution for n = 2 ou n = 3.")
+        raise ValueError("No Solution for n = 2 or n = 3.")
     
     r = n % 6
     positions = []  
@@ -21,7 +21,7 @@ def initial_solution(n):
             positions.append((2 * i, i))  
         for i in range(1, half + 1):
             positions.append((2 * i - 1, half + i))
-        print("The use of the first class: A (A₁ et A₂)")
+        print("The use of the first class: A (A1 and A2)")
 
     # second case : n = 6k+1 ou n = 6k+5
     elif r in [1, 5]:
@@ -31,7 +31,7 @@ def initial_solution(n):
             positions.append((2 * i, i + 1))  # B2 = {(2i, i+1)}
         for i in range(1, half + 1):
             positions.append((2 * i - 1, half + i + 1))  # B3 = {(2i-1, (n+1)/2 + i)}
-        print("The use of the second class B (B₁, B₂ et B₃)")
+        print("The use of the second class B (B1, B2 and B3)")
 
     # third case: n = 6k+2
     elif r == 2:
@@ -44,7 +44,7 @@ def initial_solution(n):
         # C8 
         for i in range(1, half - 2):
             positions.append((n - 2 * i - 3, half + i + 2))
-        print("The use of the third class C (C₁ à C₈)")
+        print("The use of the third class C (C1 to C8)")
 
     # Fourth case : n = 6k+3
     elif r == 3:
@@ -54,7 +54,7 @@ def initial_solution(n):
                 if smaller_board[i, j] == 1:
                     positions.append((i + 1, j + 1))
         positions.append((n, n))  # add the queen in the top right
-        print("The use of the fourth class D")
+        print("The use of the fourth class ")
 
     # fulling the matrix by the indexes calculated
     for row, col in positions:
